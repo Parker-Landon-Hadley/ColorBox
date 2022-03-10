97450adbc659d14492bdd5e8465f4202654392d3
ParkerHadley <parker.l.hadley@gmail.com>
2022-03-10 18:50:42 -0500

    Set timeout function so that jumping could be repeated

--- script.js
+++ script.js
@@ -2,4 +2,7 @@ let character = document.getElementById("character");
 let block = document.getElementById("block");
 function jump(){
     character.classList.add("animate");
+    setTimeout(function(){
+        character.classList.remove("animate");
+    }, 500);
 }
