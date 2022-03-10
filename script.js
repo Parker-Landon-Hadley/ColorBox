c5ef2c3c6e51733b8d19e9a24f26bfc1cb98eb5b
ParkerHadley <parker.l.hadley@gmail.com>
2022-03-10 18:54:39 -0500

    Formatting

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 let character = document.getElementById("character");
 let block = document.getElementById("block");
 function jump(){
-    character.classList.add("animate");
+    if(character.classList != "animate"){
+        character.classList.add("animate");
+    }
+    
     setTimeout(function(){
         character.classList.remove("animate");
-    }, 500);
+    }, 700);
 }
